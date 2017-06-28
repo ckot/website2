4ea84dd1271c0612b824dba41dab1a7d20d36c97
Scott Silliman <ssilliman@pitt.edu>
2017-06-28 16:45:43 -0400

    initial commit

---
+++ website2/urls.py
@@ -0,0 +1,37 @@
+"""website2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+import debug_toolbar
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/password_reset/$',
+        auth_views.password_reset,
+        name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$',
+        auth_views.password_reset_done,
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^reset/done/$',
+        auth_views.password_reset_complete),
+    url(r'^experiments/',
+        include('exp2.urls', namespace='experiments')),
+    url(r'^__debug__/', include(debug_toolbar.urls))
+]
